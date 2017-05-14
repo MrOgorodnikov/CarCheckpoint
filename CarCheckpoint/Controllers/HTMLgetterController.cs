e8ca6e997463b5e48b868f4295d25275d9da03c7
ZigHail <ogo.ogo50@gmail.com>
2017-05-14 03:33:29 +0300

    Vies

---
+++ CarCheckpoint/Controllers/HTMLgetterController.cs
@@ -0,0 +1,42 @@
+﻿using CarCheckpoint.BussinesLogic;
+using CarCheckpoint.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CarCheckpoint.Controllers
+{
+    public class HTMLgetterController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetInfo(string cardnumber)
+        {            
+            return View(AllUserData.GetSelectedUser(cardnumber));            
+        }
+
+        public ActionResult Find()
+        {
+            return View();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return View(Users.GetAllUsers());
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
